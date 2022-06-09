5ca56d2727feaccd6162b961c3af771d0522c2f7
sayeef-rahman <sayeef.cse@gmail.com>
2022-06-09 17:06:58 +0600

    home page hero section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import bg from "../../assets/bg.jpg"
+
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home</h1>
+        <div className="hero min-h-screen bg-opacity-100" style={{ backgroundImage: `url(${bg})`}}>
+            <div className="px-6">
+                <div className="text-slate-800 lg:w-1/2">
+                    <h1 className="mb-5 text-3xl font-bold ">I'm Sayeef Rahman</h1>
+                    <p className="mb-5  text-justify">Hand tools have been used by humans since the Stone Age when stones were used for hammering & cutting. In the Bronze Age tools were made by casting the copper & tin alloys. Bronze tools were sharper & harder than those made of stone.</p>
+                </div>
+            </div>
         </div>
     );
 };
