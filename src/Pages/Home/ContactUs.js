ab87383ad425a413643c54014194f6369cd06237
sayeef-rahman <sayeef.cse@gmail.com>
2022-06-09 19:15:04 +0600

    contact us from email implemented

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import emailjs from 'emailjs-com'
 
 const ContactUs = () => {
+    function sendEmail(e) {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const mobile = e.target.mobile.value;
+        const message = e.target.message.value;
+
+        console.log(name, mobile, email, message);
+        
+        emailjs.sendForm('service_g8sbfh5',
+            'template_dxr1ktu',
+            e.target,
+            '4kVPFcUi8ScF-1AY6'
+            ).then(res =>{
+                console.log(res);
+            }).catch(error =>{
+                console.log(error);
+            })
+    }
     return (
         <div className="hero bg-blend-lighten lg:w-3/4 mx-auto mb-10">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -10,27 +30,35 @@ const ContactUs = () => {
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Mobile</span>
-                            </label>
-                            <input type="number" placeholder="Mobile" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Queries</span>
-                            </label>
-                            <textarea className="textarea textarea-info" placeholder="Bio"></textarea>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary text-white">SEND MESSAGE</button>
-                        </div>
+                        <form onSubmit={sendEmail}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" placeholder="Your Name" name='name' className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" name='email' className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Mobile</span>
+                                </label>
+                                <input type="number" placeholder="Your Mobile Numebr" name='mobile' className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Message</span>
+                                </label>
+                                <textarea className="textarea textarea-info" placeholder="Your Message" name='message'></textarea>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary text-white" type="submit" value='SEND MESSAGE' />
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -3,28 +3,25 @@ import logo from "../../assets/logo.png"
 
 const Navbar = () => {
     return (
-        <div class="navbar bg-base-100 lg:w-3/4 mx-auto sticky">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="navbar bg-base-100 lg:w-3/4 mx-auto sticky">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         <li><a>About</a></li>
                         <li><a>Resume</a></li>
                     </ul>
                 </div>
-                <a class="btn btn-ghost normal-case text-xl w-64"><img src={logo} alt="Sayeef Rahman" /> </a>
+                <a className="btn btn-ghost normal-case text-xl w-64"><img src={logo} alt="Sayeef Rahman" /> </a>
             </div>
-            <div class="navbar-end hidden lg:flex">
-                <ul class="menu menu-horizontal p-0">
+            <div className="navbar-end hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
                     <li><a>About</a></li>
                     <li><a>Resume</a></li>
                 </ul>
             </div>
-            {/* <div class="navbar-end">
-                <a class="btn">Get started</a>
-            </div> */}
         </div>
     );
 };
