d9afc013eca94d44c328a47fac4b5f68e1881bf2
sayeef-rahman <sayeef.cse@gmail.com>
2022-06-09 17:30:34 +0600

    icon, title & resume download button added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import bg from "../../assets/bg.jpg"
+import resume from "../../assets/sayeef_resume.pdf"
 
 
 const Home = () => {
     return (
-        <div className="hero min-h-screen bg-opacity-100" style={{ backgroundImage: `url(${bg})`}}>
+        <div className="hero min-h-screen bg-opacity-100" style={{ backgroundImage: `url(${bg})` }}>
             <div className="px-6">
                 <div className="text-slate-800 lg:w-1/2">
                     <h1 className="mb-5 text-3xl font-bold ">I'm Sayeef Rahman</h1>
                     <p className="mb-5  text-justify">Hand tools have been used by humans since the Stone Age when stones were used for hammering & cutting. In the Bronze Age tools were made by casting the copper & tin alloys. Bronze tools were sharper & harder than those made of stone.</p>
+                </div>
+                <div>
+                    <a href={resume} target="_blank"><button className='btn btn-primary font-bold text-white'>Download Resume</button></a>
                 </div>
             </div>
         </div>
