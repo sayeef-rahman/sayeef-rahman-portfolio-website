c17c5a19c2a73467b9dad96732f2d29e93b38b21
sayeef-rahman <sayeef.cse@gmail.com>
2022-06-10 22:55:24 +0600

    footer added + navbar updated

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -4,24 +4,26 @@ import logo from "../../assets/logo.png"
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 lg:w-3/4 mx-auto sticky">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <div className='bg-red-600'>
+            <div className="navbar  lg:w-3/4 mx-auto sticky text-white">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a>About</a></li>
+                            <li><a>Resume</a></li>
+                        </ul>
+                    </div>
+                    <Link className="btn btn-ghost normal-case text-xl w-64" to={'/'}><img src={logo} alt="Sayeef Rahman" /> </Link>
+                </div>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
                         <li><a>About</a></li>
                         <li><a>Resume</a></li>
                     </ul>
                 </div>
-                <Link className="btn btn-ghost normal-case text-xl w-64" to={'/'}><img src={logo} alt="Sayeef Rahman" /> </Link>
-            </div>
-            <div className="navbar-end hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
-                    <li><a>About</a></li>
-                    <li><a>Resume</a></li>
-                </ul>
             </div>
         </div>
     );
