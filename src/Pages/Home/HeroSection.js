d17abde490b8d1e8d1e5fcc35520834861929791
sayeef-rahman <sayeef.cse@gmail.com>
2022-06-13 22:52:15 +0600

    Hero Section added

--- src/Pages/Home/HeroSection.js
+++ src/Pages/Home/HeroSection.js
@@ -8,7 +8,7 @@ const HeroSection = () => {
             <div className="px-6">
                 <div className="text-slate-800 lg:w-1/2">
                     <h1 className="mb-5 text-3xl font-bold ">I'm Sayeef Rahman</h1>
-                    <p className="mb-5  text-justify">Hand tools have been used by humans since the Stone Age when stones were used for hammering & cutting. In the Bronze Age tools were made by casting the copper & tin alloys. Bronze tools were sharper & harder than those made of stone.</p>
+                    <p className="mb-5  text-justify">I am a Web Developer.</p>
                 </div>
                 <div>
                     <a href={resume} target="_blank"><button className='btn btn-primary font-bold text-white rounded-none'>Download Resume</button></a>
