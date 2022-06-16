135283cd15323ba4b9789a10c01b167d92aaef05
sayeef-rahman <sayeef.cse@gmail.com>
2022-06-16 23:42:46 +0600

    footer updated

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -56,7 +56,7 @@ const Footer = () => {
                         </svg>
                     </a>
 
-                    <a href="#!" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+                    <a href="https://github.com/sayeef-rahman" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
                         <svg aria-hidden="true"
                             focusable="false"
                             data-prefix="fab"
