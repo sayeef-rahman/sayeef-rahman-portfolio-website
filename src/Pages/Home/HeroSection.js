3810f488a39012f5c90cb5b821fcf3c1f5b0efaf
sayeef-rahman <sayeef.cse@gmail.com>
2022-06-18 23:13:05 +0600

    Hero section updated

--- src/Pages/Home/HeroSection.js
+++ src/Pages/Home/HeroSection.js
@@ -8,7 +8,7 @@ const HeroSection = () => {
             <div className="px-6">
                 <div className="text-slate-800 ">
                     <h1 className="mb-5 text-3xl font-bold ">I'm Sayeef Rahman</h1>
-                    <p className="mb-5">I am a Web Developer.</p>
+                    <p className="mb-5">I am a Web Developer with 2+ years experience.</p>
                 </div>
                 <div>
                     <a href={resume} target="_blank"><button className='btn btn-primary font-bold text-white rounded-none'>Download Resume</button></a>
