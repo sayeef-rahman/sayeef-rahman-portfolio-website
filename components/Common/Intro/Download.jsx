204a5104f00d52d7a9975cd5a8ff10f6a618cefe
sayeef-rahman <sayeef.cse@gmail.com>
2024-10-05 14:43:25 +0600

    feat(intro); download resume section update

--- components/Common/Intro/Download.jsx
+++ components/Common/Intro/Download.jsx
@@ -1,8 +1,8 @@
-import Link from 'next/link';
+import Link from "next/link";
 const Download = ({ icon }) => {
   return (
     <>
-      <Link href='/sayeef-resume.pdf' target='_blank' className='flex flex-row text-LightGray items-center gap-x-4 pb-14 pt-4'>
+      <Link href='/sayeef-resume.pdf' target='_blank' className='text-LightGray flex flex-row items-center justify-center gap-x-4 pb-14 pt-4'>
         <span className='text-Snow'>Download Resume</span>
         <span>{icon}</span>
       </Link>
