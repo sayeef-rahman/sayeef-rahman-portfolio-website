0af70301dcefdc18a913921838291410f5325785
sayeef-rahman <sayeef.cse@gmail.com>
2024-10-05 12:38:16 +0600

    image name changed

--- components/Common/Intro/index.jsx
+++ components/Common/Intro/index.jsx
@@ -8,7 +8,7 @@ import Tools from './Tools';
 import Skills from './Skills';
 import Image from 'next/image';
 import { NAME, DESIGNATION, SOCIAL_LINKS } from '../../../constants/constants';
-import Sayeef from '../../../public/images/sayeef.jpg';
+import Sayeef from '../../../public/images/sayeef.jpeg';
 
 const Intro = () => {
   return (
